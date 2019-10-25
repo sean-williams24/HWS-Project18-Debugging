4ab3cc8bf1d3b75c76eaafedf4b228f20d681f3e
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-25 09:18:41 +0100

    FEAT: Test Breakpoints with conditions and exception break points

--- Project 18 - Debugging/ViewController.swift
+++ Project 18 - Debugging/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
     }
 
 
